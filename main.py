56821ce3991b981121ce60792168ac25272e5ee5
Trinh Nguyen <nguyentrinh150221@gmail.com>
2025-05-12 02:45:53 -0700

    Update Title

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from daily_app import daily_tracking_app
 from cpc_app import cpc_dashboard_app
 
 # Cấu hình giao diện
-st.set_page_config(page_title="📈 Unified Tracking App", layout="wide")
+st.set_page_config(page_title="📈 Data Integration Application", layout="wide")
 
 # Menu chính
 st.sidebar.title("📌 Menu")
